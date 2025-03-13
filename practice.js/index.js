47d0a1ccf296bc28d318161f82ae9de4e6519233
Raja <kraj9380286@gmail.com>
2025-03-13 14:14:27 +0530

    commited

--- practice.js/index.js
+++ practice.js/index.js
@@ -1,7 +1,17 @@
-let arr =[2,4,6,8]
+// let arr =[2,4,6,8]
 
-let ARRAY=arr.map((number)=>{
-    return number+number
-})
+// let ARRAY=arr.map((number)=>{
+//     return number+number
+// })
  
-console.log(ARRAY);
+// console.log(ARRAY);
+let Arr=[4,1,2,8,9,3,5,68]
+ let newArr=Arr.filter((number)=>{
+    if(number%2===0){
+        return true;
+    }
+    else{
+        return false;
+    }
+})
+console.log(newArr);
